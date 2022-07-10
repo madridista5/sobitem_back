e0bb2ac331056f77a5fbaa2553484756939dfe92
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-07-10 16:24:30 +0200

    Database was configured.

---
+++ src/config/db.config.ts
@@ -0,0 +1,14 @@
+import { registerAs } from '@nestjs/config';
+
+export default registerAs('database', () => ({
+  type: 'mysql',
+  logging: true,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: ['dist/**/**.entity{.ts,.js}'],
+  bigNumberStrings: false,
+  synchronize: true,
+}));
